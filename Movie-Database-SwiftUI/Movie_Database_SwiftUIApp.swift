d22b82a0b85541a8e5e478ad926153658cf79ad3
Charan Ganesh <charan@onetapcheckin.com>
2024-10-27 22:59:27 +0530

    Initial Commit

---
+++ Movie-Database-SwiftUI/Movie_Database_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Database_SwiftUIApp.swift
+//  Movie-Database-SwiftUI
+//
+//  Created by Charan Ganesh on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Database_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
